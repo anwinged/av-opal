654b902cd4fbcf38842d1f040b7f7193170689ca
anwinged <anwinged@c733b785-2984-70f4-63a9-da4130069974>
2012-03-10 11:44:08 +0000

    Base class structure for application

---
+++ trunk/opal.py
@@ -0,0 +1,78 @@
+﻿#!/usr/bin/env python
+#! coding: utf-8
+
+import subprocess
+
+#-------------------------------------------------------------------------------
+
+class LocalServer:
+    """
+    """
+    def __init__(self):
+        self.max_run = 2
+        self.cur_run = 0
+
+        self.task_queue = []
+
+    def GetTaskDescriptionList(self):
+        pass
+
+    def GetTaskCount(self):
+        pass
+
+    def GetTask(self, index):
+        pass
+
+    def AddTask(self, *args):
+        pass
+
+#-------------------------------------------------------------------------------
+
+class TaskDescription:
+    """
+    Description of the task. Task runs on server.
+    """
+    def __init__(self, server, tid, data):
+        self.server = server
+        self.tid = tid
+        self.data = data
+
+#-------------------------------------------------------------------------------
+
+class ObjectDescription:
+    def __init__(self, taskdescr, label, data):
+        pass
+
+class ObjectDefinition:
+    def __init__(self, taskdescr, label, data):
+        pass
+
+class Parameter:
+    def __init__(self, paramdescr):
+        pass
+
+#-------------------------------------------------------------------------------
+
+class Task:
+    def __init__(self, server):
+        pass
+
+    def Start(self):
+        pass
+
+    def Stop(self):
+        pass
+
+    def Pause(self):
+        pass
+
+    def Status(self):
+        pass
+
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- trunk/tasks/task.js
+++ trunk/tasks/task.js
@@ -1,32 +1,56 @@
 {
     "title": "Example task",
+    "author": "Anton Vakhrushev",
 
-    "meta": {
-        "silent": true,
-    }
-
-    "models": [
-        {
-            "title": "Example model",
+    "silent": false,
+    
+    "models": {
+    
+        "simpleexample": {
+        
+            "title": "Simple example model",
 
             "data": {
+            
                 "x": {
                     "type":     "int",
                     "default":  10
+                },
+                
+                "u": {
+                    "type":     "double",
+                    "default":  3.14
                 }
             },
 
-            "methods": [
-                {
-                    "title": "default",
+            "methods": {
+            
+                "default": {
+                    "title": "Default method",
                     "data": {
-                        "u": {
+                        "p": {
                             "type":     "int",
                             "default":  20
                         }
                     }
                 }
-            ]
+            },
+            
+            "result": {
+                
+                "data": {
+                    "sum": "int"
+                },
+                
+                "table": {
+                    "head": [
+                        { "x" : "int" },
+                        { "u" : "double" }
+                    ]
+                }
+                
+            }
         }
-    ]
+    }
 }
+

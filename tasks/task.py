cd0f4f316a1561a825413d6f97ee9f8da3762253
anwinged <anwinged@c733b785-2984-70f4-63a9-da4130069974>
2012-03-10 11:44:08 +0000

    Base class structure for application

--- tasks/task.py
+++ tasks/task.py
@@ -5,18 +5,18 @@ import json
 
 def main():
 
-    if len(sys.argv) != 2:
-        print 'Error!'
-        return
+##    if len(sys.argv) != 2:
+##        print 'Error!'
+##        return
+##
+##    if sys.argv[1] == '-i':
+    with open('task.js') as f:
+        d = json.load(f)
+        print json.dumps(d, indent = 2)
 
-    if sys.argv[1] == '-i':
-        with open('task.js') as f:
-            d = json.load(f)
-            print json.dumps(d)
-
-    elif sys.argv[1] == '-r':
-        data = raw_input()
-        data = json.loads(data)
+##    elif sys.argv[1] == '-r':
+##        data = raw_input()
+##        data = json.loads(data)
 
 if __name__ == '__main__':
     main()
